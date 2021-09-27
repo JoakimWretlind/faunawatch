4835df9a10a8b5a46c67b1b2a049a9d380da4e16
Joakim Wretlind <joakim.wretlind@gmail.com>
2021-09-27 17:53:17 +0200

    new fonts

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,6 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
+import CabinSketchB from './components/assets/fonts/CabinSketch-Bold.ttf';
+import SourceSansLight from './components/assets/fonts/SourceSansPro-Light.ttf';
+import AvenirBlack from './components/assets/fonts/Avenir-Black.otf';
+import ClashBold from './components/assets/fonts/ClashDisplay-Bold.ttf';
+import EsperaBold from './components/assets/fonts/Espera-Bold.ttf';
+import EsperaRegular from './components/assets/fonts/Espera-Regular.ttf';
+
 export const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'cabin-bold';
+  src: url(${CabinSketchB});
+}
+@font-face {
+  font-family: 'source-light';
+  src: url(${SourceSansLight});
+}
+@font-face {
+  font-family: 'avenir-black';
+  src: url(${AvenirBlack});
+}
+@font-face {
+  font-family: 'clashBold';
+  src: url(${ClashBold});
+}
+@font-face {
+  font-family: 'EsperaBold';
+  src: url(${EsperaBold});
+}
+@font-face {
+  font-family: 'EsperaRegular';
+  src: url(${EsperaRegular});
+}
     *,
     ::after,
     ::before {
@@ -15,7 +46,8 @@ export const GlobalStyle = createGlobalStyle`
     
     body {
       box-sizing: border-box;
-      font-family: 'poppins', sans-serif;
+      // font-family: 'poppins', sans-serif;
+      font-family: 'EsperaRegular';
     }
     
     a {
@@ -24,5 +56,11 @@ export const GlobalStyle = createGlobalStyle`
     
     ul, li {
       list-style: none;
+    }
+    h1{
+      font-family: 'cabin-bold';
+    }
+    h2{
+      font-family: 'EsperaBold';
     }
 `;
